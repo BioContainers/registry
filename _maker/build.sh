099ea4215db91e09ad2b96b256e4084db7907849
Yasset Perez-Riverol <ypriverol@gmail.com>
2016-06-02 22:52:50 +0100

    delted other things

--- _maker/build.sh
+++
@@ -1,3 +0,0 @@
-source setup
-
-docker run --rm -v "$CFG:/src" --name ${NAME}_build grahamc/jekyll build
--- _maker/cmd.sh
+++
@@ -1,5 +0,0 @@
-CMD=$1
-
-source config
-
-docker run --rm -v "$CFG:/src" --name ${NAME}_run grahamc/jekyll rake $CMD
