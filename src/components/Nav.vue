6bcf97c54b0cb45765f94dc926206a60ddc7f877
yperez <ypriverol@gmail.com>
2019-03-06 15:52:58 +0000

    minor changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,18 +3,43 @@
         <Menu mode="horizontal" active-name="1" @on-select="menuSlect">
             <a class="logo" @click="gotoHome"><img src="static/logo/biocontainers-logo.png" width="40" height="40">BioContainers</a>
             <div class="menu">
-                <MenuItem name="multipackage">
-                    <Icon type="ios-add-circle"></Icon>
-                    Multi-package
+                <MenuItem name="registry">
+                    <Icon type="ios-search"></Icon>
+                    Registry
                 </MenuItem>
-                <MenuItem name="statslink">
-                   <Icon type="ios-home-outline"></Icon>
-                    Statistics
+                <MenuItem name="documentation">
+                    <Icon type="ios-information-circle-outline"></Icon>
+                    Documentation
                 </MenuItem>
-                <MenuItem name="biocontainers">
-                    <Icon type="ios-home-outline"></Icon>
-                    BioContainers
-                </MenuItem>
+                <Dropdown>
+                    <a href="javascript:void(0)">
+                        <Icon type="ios-arrow-down"></Icon>
+                        Resources
+                    </a>
+                    <DropdownMenu slot="list">
+                        <DropdownItem name="statslink">
+                             <Icon type="ios-home-outline"></Icon>
+                             Statistics
+                        </DropdownItem>
+                        <DropdownItem name="multipackage">
+                            <Icon type="ios-add-circle"></Icon>
+                            Create Multi-tools container
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+
+                <!--<MenuItem name="multipackage">-->
+                    <!--<Icon type="ios-add-circle"></Icon>-->
+                    <!--Multi-package-->
+                <!--</MenuItem>-->
+                <!--<MenuItem name="statslink">-->
+                   <!--<Icon type="ios-home-outline"></Icon>-->
+                    <!--Statistics-->
+                <!--</MenuItem>-->
+                <!--<MenuItem name="biocontainers">-->
+                    <!--<Icon type="ios-home-outline"></Icon>-->
+                    <!--BioContainers-->
+                <!--</MenuItem>-->
                 <MenuItem name="github">
                     <Icon type="logo-github"></Icon>
                     GitHub
@@ -44,32 +69,36 @@
             },
             menuSlect(name){
                 switch(name){
+                    case 'registry':
+                        this.$router.push({name:'Registry'});
+                        break;
+                    case 'documentation':
+                        window.open('http://biocontainers-edu.biocontainers.pro');
+                        break;
                   case 'statslink':
-                    this.$router.push({name:'Mappingdata'});
-                    //this.activeName = 'index';
-                  break;
-
+                      this.$router.push({name:'Mappingdata'});
+                      break;
                   case 'multipackage':
                     this.$router.push({name:'Multipackage'});
                     //window.open('http://biocontainers.pro/multi-package-containers/');
                     //this.activeName = 'discover';
-                  break;
+                    break;
                   case 'biocontainers':
                     //this.$router.push({ name: 'discover', params: { category: '全部'},query:{sort: "popular"}});
                     //this.activeName = 'discover';
-                   window.open('http://biocontainers.pro')
-                  break;
+                    window.open('http://biocontainers.pro')
+                    break;
                   case 'github':
                     //this.$router.push({ name: 'discover', params: { category: '全部'},query:{sort: "popular"}});
                     //this.activeName = 'discover';
-                      window.open('http://github.com/Biocontainers')
-                  break;
+                     window.open('http://github.com/Biocontainers')
+                     break;
                   case 'twitter':
                       window.open('https://twitter.com/BioContainers')
-                  break;
+                      break;
                   case 'scholar':
                       window.open('https://scholar.google.co.uk/citations?user=O6xG2-EAAAAJ')
-                  breask;
+                      break;
                 }
             } 
         }
