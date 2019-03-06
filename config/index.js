2cf0a181241955766f66910968ede001a4514c3e
yperez <ypriverol@gmail.com>
2019-03-06 14:10:53 +0000

    minor changes

--- config/index.js
+++ config/index.js
@@ -53,7 +53,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
    // Paths
-   assetsRoot: path.resolve(__dirname, '../'),
+   assetsRoot: path.resolve(__dirname, '../dist'),
    assetsSubDirectory: 'static',
    assetsPublicPath: '/dist/',
 
