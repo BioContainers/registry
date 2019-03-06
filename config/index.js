8c8f4fe1b1c9fb1e28af41b0dde2123c4fd3d944
yperez <ypriverol@gmail.com>
2019-03-06 10:28:37 +0000

    minor changes

--- config/index.js
+++ config/index.js
@@ -50,13 +50,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../registry/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../'),
-    assetsSubDirectory: 'static',
-    //assetsPublicPath: '/',
-    assetsPublicPath: '/',
+   // Paths
+   assetsRoot: path.resolve(__dirname, '../dist'),
+   assetsSubDirectory: 'static',
+   assetsPublicPath: '/dist/',
 
     /**
      * Source Maps
